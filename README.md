c277daaebe87062f8e938d6798bb3fb191682f4c
MicaellyUchoa <micaellyit@hotmail.com>
2023-04-19 22:46:25 -0300

    feat: adding unit tests and remove coverage folder

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# playground-testing-with-jest
+# Playground - Testing with Jest
+
+Para instalar as dependências
+```
+npm install
+```
+
+Para rodar o projeto: 
+```
+npm run dev
+```
+
+
+Para rodar os testes unitários:
+
+```
+test:unit
+```
+
+Para rodar os testes unitários com ouvinte de alterações:
+
+```
+test:watch
+```
+
--- src/components/UseStateTesting/index.test.tsx
+++ src/components/UseStateTesting/index.test.tsx
@@ -1,14 +1,14 @@
-import  { Dispatch } from "react";
+import { Dispatch } from "react";
 import * as React from "react";
 import { render, screen } from "@testing-library/react";
 
 import { UseStateTesting } from ".";
 
-const setIsLoading: Dispatch<unknown> = jest.fn();
+const setState: Dispatch<unknown> = jest.fn();
 describe("Component <UseStateTesting/>", () => {
   it("should be called state loading (true)", () => {
     const isLoading: unknown = true;
-    const useStateSpy = jest.spyOn(React, "useState").mockReturnValue([isLoading, setIsLoading]);
+    const useStateSpy = jest.spyOn(React, "useState").mockReturnValue([isLoading, setState]);
 
     render(<UseStateTesting />);
 
@@ -18,11 +18,31 @@ describe("Component <UseStateTesting/>", () => {
 
   it("should be called state loading (false)", () => {
     const isLoading: unknown = false;
-    const useStateSpy = jest.spyOn(React, "useState").mockReturnValue([isLoading, setIsLoading]);
+    const useStateSpy = jest.spyOn(React, "useState").mockReturnValue([isLoading, setState]);
 
     render(<UseStateTesting />);
 
     expect(screen.getByText("The page has already been loaded!")).toBeInTheDocument();
+    expect(useStateSpy).toHaveBeenCalled();
+  });
+
+  it('should be called state isTesting with text "testing"', () => {
+    const isTesting: string = "testing";
+    const useStateSpy = jest.spyOn(React, "useState").mockReturnValue([isTesting, setState]);
+
+    render(<UseStateTesting />);
+
+    expect(screen.getByText("We are testing!")).toBeInTheDocument();
+    expect(useStateSpy).toHaveBeenCalled();
+  });
+
+    it('should be called state isTesting with text "tested"', () => {
+    const isTesting: string = "tested";
+    const useStateSpy = jest.spyOn(React, "useState").mockReturnValue([isTesting, setState]);
+
+    render(<UseStateTesting />);
+
+    expect(screen.getByText("It was tested!")).toBeInTheDocument();
     expect(useStateSpy).toHaveBeenCalled();
   });
 });
